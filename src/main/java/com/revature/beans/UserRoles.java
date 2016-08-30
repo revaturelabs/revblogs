6882806af6e0088448c44644905118897427eab6
Patrick Zebron <patz1@umbc.edu>
2016-08-30 09:09:29 -0400

    #done #comment Beans and Mapping Completed #time 3hrs

--- src/main/java/com/revature/beans/UserRoles.java
+++ src/main/java/com/revature/beans/UserRoles.java
@@ -1,14 +1,58 @@
 package com.revature.beans;
 
+import javax.persistence.*;
+
 /**
  * Users can be of role:
  * 		CONTRIBUTOR, ADMIN
- *
  */
-public class UserRoles {
 
+@Entity
+@Table(name="REV_BLOG_USER_ROLES")
+public class UserRoles {
+	
+		@Id
+		@Column(name="REV_BLOG_USER_ROLE_ID")
 	private int userRoleId;
-	private String username;
+		@Column(name="REV_BLOG_ROLE", nullable=false)
 	private String role;
+				
+	/**
+	 *  Constructors
+	 */
+		
+	public UserRoles() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+		
+	public UserRoles(int userRoleId, String role) {
+		super();
+		this.userRoleId = userRoleId;
+		this.role = role;
+	}
+	
+	/**
+	 *  Getters & Setters
+	 */
+
+	public int getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(int userRoleId) {
+		this.userRoleId = userRoleId;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+	
 	
 }
