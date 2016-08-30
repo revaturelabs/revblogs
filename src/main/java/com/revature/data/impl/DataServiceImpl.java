683035bdf842de3c772da62c9ac44d34b252267c
Justin Wheeler <wheeler146@yahoo.com>
2016-08-30 10:48:00 -0400

    #done #comment DAO/DataService/BusinessDelegate #time 30m

--- src/main/java/com/revature/data/impl/DataServiceImpl.java
+++ src/main/java/com/revature/data/impl/DataServiceImpl.java
@@ -1,5 +1,12 @@
 package com.revature.data.impl;
 
+import java.util.List;
+
+import com.revature.beans.Blog;
+import com.revature.beans.Evidence;
+import com.revature.beans.Tags;
+import com.revature.beans.User;
+import com.revature.beans.UserRoles;
 import com.revature.data.DAO;
 import com.revature.data.DataService;
 
@@ -11,4 +18,29 @@ public class DataServiceImpl implements DataService{
 		this.dao = dao;
 	}
 	
+	// Push
+	public void makeRecord(Object _obj){
+		dao.insertRecord(_obj);
+	}
+	
+	// Pull
+	public User grabUsers(String _username){
+		return dao.getUsers(_username);
+	}
+	
+	public List<User> grabUsers(){
+		return dao.getUsers();
+	}
+	public List<Blog> grabBlogs(){
+		return dao.getBlogs();
+	}
+	public List<Tags> grabTags(){
+		return dao.getTags();
+	}
+	public List<UserRoles> grabRoles(){
+		return dao.getRoles();
+	}
+	public List<Evidence> grabEvidence(){
+		return dao.getEvidence();
+	}
 }
--- src/main/java/com/revature/service/BusinessDelegate.java
+++ src/main/java/com/revature/service/BusinessDelegate.java
@@ -1,5 +1,24 @@
 package com.revature.service;
 
-public interface BusinessDelegate {
+import java.util.List;
 
+import com.revature.beans.Blog;
+import com.revature.beans.Evidence;
+import com.revature.beans.Tags;
+import com.revature.beans.User;
+import com.revature.beans.UserRoles;
+
+public interface BusinessDelegate {
+	
+	// Push
+	public void putRecord(Object _obj);
+	
+	// Pull
+	public User requestUsers(String _username);
+	
+	public List<User> requestUsers();
+	public List<Blog> requestBlogs();
+	public List<Tags> requestTags();
+	public List<UserRoles> requestRoles();
+	public List<Evidence> requestEvidence();
 }
