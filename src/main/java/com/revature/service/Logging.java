bd4dcd737c82b9475623566fb37992019b962d00
wjandrz <wjandrz@earthlink.net>
2016-08-30 14:12:57 -0400

    REV-19 #done #comment logger for debugging #time 45m

---
+++ src/main/java/com/revature/service/Logging.java
@@ -0,0 +1,45 @@
+package com.revature.service;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Logger for debugging
+ */
+
+@Aspect
+public class Logging {
+	
+	private static Logger log = Logger.getRootLogger(); 
+	
+	@Around("logPerformance()")
+	public Object intercept(ProceedingJoinPoint pjp){
+		
+		//Object to eventually be sent temporarily null
+		Object result = null;
+		
+		try {
+			//Send to the logger a debug message that the method is being executed
+			log.debug("Executed Method (" + pjp.getSignature()+ ")!" );
+			
+			//Execute the intercepted method
+			result = pjp.proceed(); 
+			
+			//Send to the logger an info message that the method succeeded
+			log.info("Returned: " + result);
+		} catch (Throwable e) {
+			//Send to the logger an error message that the method failed
+			log.error(e);
+		}
+		//Send null or the actual result back to the chain
+		return result;
+	}
+	
+	 //------------------POINTCUTS------------------//
+	//Whenever any method is executed
+	@Pointcut("execution(* *(..))")
+	public void logPerformance(){}
+}
