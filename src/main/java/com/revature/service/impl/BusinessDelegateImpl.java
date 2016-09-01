46f022cf654f8d2b977e22d339052ebb577d9911
jjf28 <forsbergjj@gmail.com>
2016-09-01 09:56:45 -0400

    more file upload specifics

--- src/main/java/com/revature/service/impl/BusinessDelegateImpl.java
+++ src/main/java/com/revature/service/impl/BusinessDelegateImpl.java
@@ -34,9 +34,9 @@ public class BusinessDelegateImpl implements BusinessDelegate{
 	}
 	
 	/**
-	 * Attempts to upload a resource (such as an image) to the S3 server
+	 * Attempts to upload a resource (such as a CSS or JS file) to the S3 server
 	 * @param fileName the destination name of the file, a valid extension should be included
-	 * @param file a file that is to be uploaded to the database
+	 * @param file a file that is to be uploaded to the S3 server
 	 * @return the URL where the file was uploaded if successful, null otherwise
 	 */
 	public String uploadResource(String fileName, MultipartFile file) {
@@ -44,12 +44,22 @@ public class BusinessDelegateImpl implements BusinessDelegate{
 	}
 	
 	/**
-	 * Attempts to upload a front-end page to the S3 server
+	 * Attempts to upload a front-end page (html) to the S3 server
 	 * @param file a file that is to be uploaded to the database, the file should have a valid extension
 	 * @return the URL where the file was uploaded if successful, null otherwise
 	 */
 	public String uploadPage(File file) {
 		return jetS3.uploadPage(file);
+	}
+	
+	/**
+	 * Attempts to upload an 'evidence' (picture, code, attachment) to the S3 server
+	 * @param fileName the destination name of the file, a valid extension should be included
+	 * @param file a file that is to be uploaded to the S3 server
+	 * @return the URL where the file was uploaded if successful, null otherwise
+	 */
+	public String uploadEvidence(String fileName, MultipartFile file) {
+		return jetS3.uploadEvidence(fileName, file);
 	}
 	
 	// Push
