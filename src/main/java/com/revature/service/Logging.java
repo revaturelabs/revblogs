241f11b111c12c3d1d3e472a9ec1150d83200aed
AlexHajek <alexanderjhajek@gmail.com>
2016-09-02 10:04:25 -0400

    Vulnerability fixed?

--- src/main/java/com/revature/service/Logging.java
+++ src/main/java/com/revature/service/Logging.java
@@ -15,27 +15,27 @@ public class Logging {
 	
 	private static Logger log = Logger.getRootLogger(); 
 	
-	@Around("logPerformance()")
-	public Object intercept(ProceedingJoinPoint pjp){
-		//Object to eventually be sent temporarily null
-		Object result = null;
-		
-		try {
-			//Send to the logger a debug message that the method is being executed
-			log.debug("Executed Method (" + pjp.getSignature()+ ")!" );
-			
-			//Execute the intercepted method
-			result = pjp.proceed(); 
-			
-			//Send to the logger an info message that the method succeeded
-			log.info("Returned: " + result);
-		} catch (Throwable e) {
-			//Send to the logger an error message that the method failed
-			log.error(e);
-		}
-		//Send null or the actual result back to the chain
-		return result;
-	}
+//	@Around("logPerformance()")
+//	public Object intercept(ProceedingJoinPoint pjp){
+//		//Object to eventually be sent temporarily null
+//		Object result = null;
+//		
+//		try {
+//			//Send to the logger a debug message that the method is being executed
+//			log.debug("Executed Method (" + pjp.getSignature()+ ")!" );
+//			
+//			//Execute the intercepted method
+//			result = pjp.proceed(); 
+//			
+//			//Send to the logger an info message that the method succeeded
+//			log.info("Returned: " + result);
+//		} catch (Throwable e) {
+//			//Send to the logger an error message that the method failed
+//			log.error(e);
+//		}
+//		//Send null or the actual result back to the chain
+//		return result;
+//	}
 	
 	//Temporary Logging functions for Bugs on SonarQube
 	//Remove if necessary
