5575eb53a22e31176d9a41d7e055d63659d96668
Luke Boggs <lukeboggs7@gmail.com>
2016-09-02 11:55:44 -0400

    blog post uploaded to S3!!

--- src/main/java/com/revature/controllers/BaseController.java
+++ src/main/java/com/revature/controllers/BaseController.java
@@ -141,16 +141,7 @@ public class BaseController {
 		blog.setPublishDate(new Date());
 		
 		businessDelegate.putRecord(blog);
-		HtmlWriter htmlWriter;
-		try {
-			InputStream templateStream = this.getClass().getClassLoader().getResourceAsStream("template.html");
-			htmlWriter = new HtmlWriter(blog, blog.getAuthor(), templateStream);
-			TemporaryFile blogTempFile = htmlWriter.render();
-			blogTempFile.destroy();
-			req.setAttribute("blog", blog);
-		} catch (FileNotFoundException e) {
-		} catch (IOException e1) {
-		}
+		req.getSession().setAttribute("blog", blog);
 		return "preview-blog";
 	}
 	
