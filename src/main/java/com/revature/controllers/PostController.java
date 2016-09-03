111d0bb3f448fad619f817b1df9acbc7b7c96f9d
Justin Wheeler <wheeler146@yahoo.com>
2016-09-03 14:12:21 -0400

    Removed Duplicate Mapping

--- src/main/java/com/revature/controllers/PostController.java
+++ src/main/java/com/revature/controllers/PostController.java
@@ -17,12 +17,12 @@ import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.revature.app.TemporaryFile;
