f06f707794325236d077cf2d28db6f89c66c216b
Eric <mr.eric.erwin@gmail.com>
2016-09-07 17:20:37 -0400

    #done #comment added security to new pages.

--- src/test/java/com/revature/DaoTest/DAOTest.java
+++ src/test/java/com/revature/DaoTest/DAOTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
