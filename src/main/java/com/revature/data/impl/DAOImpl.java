b22f08d77453efc981118b7212a705370b24c310
AlexHajek <alexanderjhajek@gmail.com>
2016-09-07 18:24:03 -0400

    Deleted SYSO's

--- src/main/java/com/revature/data/impl/DAOImpl.java
+++ src/main/java/com/revature/data/impl/DAOImpl.java
@@ -107,12 +107,10 @@ public class DAOImpl implements DAO {
 	// Delete or Archive
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void deleteRecord(Object obj){
-		System.out.println("About to Falsify");
 		Session ses = sessionFactory.getCurrentSession();
 		setSession(ses);
 		
 		if(obj instanceof Blog){
-			System.out.println("Falsify");
 			// Archive
 			((Blog)obj).setActive(false);
 			editRecord(obj);
