4ae9b01e9a0dba18d4efdc62f38902bc4d8821d9
Trey McDeane <tmcdeane@gmail.com>
2016-09-08 13:47:08 -0400

    allowed cross-origin requests to AJAX controller

--- src/main/java/com/revature/controllers/AjaxController.java
+++ src/main/java/com/revature/controllers/AjaxController.java
@@ -1,10 +1,15 @@
 package com.revature.controllers;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,15 +32,18 @@ public class AjaxController {
 		this.businessDelegate = businessDelegate;
 	}
 	
-	@RequestMapping(value="/posts")
+	@RequestMapping(value="/posts", method=RequestMethod.GET)
 	@ResponseBody
-	public BlogPostCollectionDTO getPosts (
+	public ResponseEntity<BlogPostCollectionDTO> getPosts (
 			@RequestParam(value="page", required=false, defaultValue="1") int page,
 			@RequestParam(value="per_page", required=false, defaultValue="10") int perPage,
 			@RequestParam(value="author", required=false, defaultValue="0") int authorId,
 			@RequestParam(value="category", required=false, defaultValue="0") int tagId,
 			@RequestParam(value="q", required=false) String searchQuery,
 			HttpServletRequest request) {
+			
+		BlogPostCollectionDTO responseObject = null;
+		
 		try {
 			log.error(authorId);
 			
@@ -44,21 +52,41 @@ public class AjaxController {
 			
 			Tags category = businessDelegate.requestTag(tagId);
 			
+			
 			if (author != null) {
-				return businessDelegate.requestBlogPosts(author, page, perPage);
+				responseObject = businessDelegate.requestBlogPosts(author, page, perPage);
 			}
 			else if (category != null) {
-				return businessDelegate.requestBlogPosts(category, page, perPage);
+				responseObject = businessDelegate.requestBlogPosts(category, page, perPage);
 			}
 			else if (searchQuery != null) {
-				return businessDelegate.searchBlogPosts(searchQuery, page, perPage);
+				responseObject = businessDelegate.searchBlogPosts(searchQuery, page, perPage);
 			}
 			else {
-				return businessDelegate.requestBlogPosts(page, perPage);
+				responseObject = businessDelegate.requestBlogPosts(page, perPage);
 			}
 		} catch (IllegalArgumentException e) {
 			Logging.info(e);
 			return null;
 		}
+		
+		HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*"); // TODO: Change this to production S3 domain name
+		
+		ResponseEntity<BlogPostCollectionDTO> entity = new ResponseEntity<BlogPostCollectionDTO>(responseObject, headers, HttpStatus.OK);
+		
+		return entity;
+	}
+	
+	@RequestMapping(value="/posts", method=RequestMethod.OPTIONS)
+	@ResponseBody
+	public ResponseEntity<BlogPostCollectionDTO> getPostsPreflight (HttpServletRequest request, HttpServletResponse response) {
+		HttpHeaders headers = new HttpHeaders();
+		
+        headers.add("Access-Control-Allow-Origin", "*"); // TODO: Change this to production S3 domain name
+        headers.add("Access-Control-Allow-Methods", "GET");
+        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        headers.add("Access-Control-Allow-Max-Age", "1800");
+        return new ResponseEntity<BlogPostCollectionDTO>(headers, HttpStatus.OK);
 	}
 }
