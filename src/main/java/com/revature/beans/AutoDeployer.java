fbc6363f01cede8fde94b196498276259fe7705b
Robo-Ky-revature <bookofbromon@gmail.com>
2016-09-09 16:15:21 -0400

    ad

--- src/main/java/com/revature/beans/AutoDeployer.java
+++ src/main/java/com/revature/beans/AutoDeployer.java
@@ -12,9 +12,10 @@ public class AutoDeployer implements InitializingBean {
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		JetS3 deployer = new JetS3Impl();
-		deployer.uploadPage(new File("src/main/webapp/WEB-INF/pages/index.html"));
-		deployer.uploadPage(new File("src/main/webapp/WEB-INF/pages/error.html"));
-		deployer.uploadPage(new File("src/main/webapp/WEB-INF/pages/search.html"));
+		deployer.uploadPage(new File("index.html"));
+		deployer.uploadPage(new File("error.html"));
+		deployer.uploadPage(new File("search.html"));
+		System.out.println("auto deployer");
 	}
 
 }
