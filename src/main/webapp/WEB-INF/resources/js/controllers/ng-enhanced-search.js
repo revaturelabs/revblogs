93a9c71735e379de0a8acdf616dc98d2f8021850
jjf28 <forsbergjj@gmail.com>
2016-09-30 12:34:55 -0400

    re-inserting enhanced search updates

--- src/main/webapp/WEB-INF/resources/js/controllers/ng-enhanced-search.js
+++ src/main/webapp/WEB-INF/resources/js/controllers/ng-enhanced-search.js
@@ -1,18 +1,23 @@
-/**
- * 
- */
-
 var enhancedSearchModule = angular.module('enhancedSearch', []);
 
 enhancedSearchModule.controller('enhancedSearchCtrl', function($scope, $http) {
 	
 	$scope.searchText = '';
-	$scope.suggestions = [];
-	$scope.selectedSuggestion = -1;
+	$scope.suggestions = [
+		{ "displayText":"", "searchText":"" },
+		{ "displayText":"d1", "searchText":"s1" },
+		{ "displayText":"d2", "searchText":"s2" },
+		{ "displayText":"d3", "searchText":"s3" },
+		{ "displayText":"d4", "searchText":"s4" },
+		{ "displayText":"d5", "searchText":"s5" }
+	];
 	$scope.maxSuggestions = 6;
+	$scope.selectedSuggestionId = 0;
+	$scope.selectedSuggestion = $scope.suggestions[0].searchText;
+	$scope.suggestionsShown = false;
 	
-	$scope.keyup = function(event) {
-		var keyCode = event.which || event.keyCode;
+	$scope.keyup = function(keyUpEvent) {
+		var keyCode = keyUpEvent.which || keyUpEvent.keyCode;
 		if ( keyCode == 38 ) {
 			$scope.upArrow();
 		} else if ( keyCode == 40 ) {
@@ -31,24 +36,24 @@ enhancedSearchModule.controller('enhancedSearchCtrl', function($scope, $http) {
 	//////////////////////////////////
 	$scope.upArrow = function() {
 		if ( $scope.searchText.length > 0 ) {
-			if ( $scope.selectedSuggestion <= 0 ) {
-				$scope.selectedSuggestion = 0;
+			if ( $scope.selectedSuggestionId <= 0 ) {
+				$scope.selectedSuggestionId = 0;
 			} else {
-				$scope.selectedSuggestion -= 1;
+				$scope.selectedSuggestionId -= 1;
 			}
-			$scope.selectSuggestion($scope.selectedSuggestion);
+			$scope.selectSuggestion($scope.selectedSuggestionId);
 			$scope.setSuggestionsVisible(true);
 		}
 	}
 	
 	$scope.downArrow = function() {
 		if ( $scope.searchText.length > 0 ) {
-			if ( $scope.selectedSuggestion >= $scope.maxSuggestions-1 ) {
-				$scope.selectedSuggestion = $scope.maxSuggestions-1;
+			if ( $scope.selectedSuggestionId >= $scope.maxSuggestions-1 ) {
+				$scope.selectedSuggestionId = $scope.maxSuggestions-1;
 			} else {
-				$scope.selectedSuggestion += 1;
+				$scope.selectedSuggestionId += 1;
 			}
-			$scope.selectSuggestion($scope.selectedSuggestion);
+			$scope.selectSuggestion($scope.selectedSuggestionId);
 			$scope.setSuggestionsVisible(true);
 		}
 	}
@@ -69,29 +74,43 @@ enhancedSearchModule.controller('enhancedSearchCtrl', function($scope, $http) {
 	///// End Key-Up Callbacks /////
 	////////////////////////////////
 	
+	$scope.selectedSuggestionChanged = function() {
+		$scope.setSuggestionsVisible(false);
+		$scope.setSearchText($scope.selectedSuggestion);
+		$scope.submitSearch($scope.selectedSuggestion);
+	}
+	
 	$scope.setSuggestionsVisible = function(visible) {
-		if ( visible ) {
-			document.getElementById("selections").style = "display: inline; z-index: 2; position: relative;";
-		} else {
-			document.getElementById("selections").style = "display: none; z-index: 2; position: relative;";
-		}
+		if ( !visible )
+			$scope.selectedSuggestionId = 0;
+		
+		$scope.suggestionsShown = !!visible;
 	}
 	
 	$scope.setSearchText = function(newText) {
 		$scope.searchText = newText;
 	}
 	
-	$scope.searchLostFocus = function() {
-		$scope.setSuggestionsVisible(false);
+	$scope.searchLostFocus = function(focusLostEvent) {
+		var itemBeingFocused = '';
+		if ( focusLostEvent != null && focusLostEvent.relatedTarget != null ) {
+			itemBeingFocused = focusLostEvent.relatedTarget.id;
+		}
+		
+		if ( itemBeingFocused != 'searchBox' && itemBeingFocused != 'selections' ) {
+			$scope.setSuggestionsVisible(false);
+		}
+	}
+	
+	$scope.isSelected = function(suggestion) {
+		return $scope.selectedSuggestion == suggestion.searchText;
 	}
 	
 	$scope.selectSuggestion = function(indexToSelect) {
 		angular.forEach($scope.suggestions, function(suggestion, index) {
-			if ( index == indexToSelect && suggestion.selected != "selected" ) {
-				suggestion.selected = "selected";
-				$scope.setSearchText(suggestion.text);
-			} else if ( index != indexToSelect && suggestion.selected != "" ) {
-				suggestion.selected = "";
+			if ( index == indexToSelect && !($scope.isSelected(suggestion)) ) {
+				$scope.selectedSuggestion = suggestion.searchText;
+				$scope.setSearchText(suggestion.searchText);
 			}
 		});
 	}
@@ -99,18 +118,19 @@ enhancedSearchModule.controller('enhancedSearchCtrl', function($scope, $http) {
 	$scope.searchTextChanged = function() {
 		var userEnteredSearchText = $scope.searchText;
 		$scope.generateSuggestions(userEnteredSearchText);
+		$scope.selectedSuggestion = $scope.suggestions[0].searchText;
 	}
 	
 	$scope.generateSuggestions = function(userEnteredSearchText) {
 		console.log("Unimplemented Generate Suggestions Method: " + userEnteredSearchText);
 		
 		$scope.suggestions = [
-			{ "text":userEnteredSearchText + "0", "selected":"" },
-			{ "text":userEnteredSearchText + "1", "selected":"" },
-			{ "text":userEnteredSearchText + "2", "selected":"" },
-			{ "text":userEnteredSearchText + "3", "selected":"" },
-			{ "text":userEnteredSearchText + "4", "selected":"" },
-			{ "text":userEnteredSearchText + "5", "selected":"" }
+			{ "displayText":userEnteredSearchText + "", "searchText":userEnteredSearchText + "" },
+			{ "displayText":userEnteredSearchText + "d1", "searchText":userEnteredSearchText + "s1" },
+			{ "displayText":userEnteredSearchText + "d2", "searchText":userEnteredSearchText + "s2" },
+			{ "displayText":userEnteredSearchText + "d3", "searchText":userEnteredSearchText + "s3" },
+			{ "displayText":userEnteredSearchText + "d4", "searchText":userEnteredSearchText + "s4" },
+			{ "displayText":userEnteredSearchText + "d5", "searchText":userEnteredSearchText + "s5" }
 		];
 	}
 	
